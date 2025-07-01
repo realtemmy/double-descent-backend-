90505a100b87a26308d58535bf7a1e39af3c8ed6
Temiloluwa <temiloluwaOgunti8@gmail.com>
2025-07-01 05:45:51 +0100

    Updated redis to cloud using upstash

--- redis.js
+++ redis.js
@@ -1,9 +1,10 @@
 const redis = require("redis");
 
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
+// const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 const client = redis.createClient({
-  url: `redis://localhost:${REDIS_PORT}`,
+  //   url: `redis://localhost:${REDIS_PORT}`,
+  url: process.env.REDIS_URL,
 });
 
 client.on("error", (err) => console.log("Redis Client Error: ", err));
